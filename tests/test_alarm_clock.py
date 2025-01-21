45c5b7e9fc44c6d23bbfa4c7bb943ae4f2fc45c2
Martin Kokot <martin.kokot@dci-student.org>
2025-01-21 14:05:13 +0100

    new project file: test_module.py & tests

---
+++ tests/test_alarm_clock.py
@@ -0,0 +1,15 @@
+import unittest
+from alarm_clock import add, subtract
+
+class TestMyModule(unittest.TestCase):
+    
+    def test_add(self):
+        self.assertEqual(add(1, 2), 3)
+        self.assertEqual(add(-1, -1), -2)
+
+    def test_subtract(self):
+        self.assertEqual(subtract(3, 2), 1)
+        self.assertEqual(subtract(5, 8), -3)
+
+if __name__ == '__main__':
+    unittest.main()
